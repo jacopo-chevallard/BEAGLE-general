1b7b03c32bc2071da8529527a94304c9285823d1
Jacopo Chevallard <jacopo.chevallard@gmail.com>
2021-03-22 14:27:38 +0100

    Adding example of env variables definition necessary to run Beagle and PyP-Beagle

---
+++ scripts/BEAGLE_env_variable.bash
@@ -0,0 +1,20 @@
+###################################################
+# START BEAGLE environment variables
+###################################################
+export BEAGLE_ROOT="${HOME}/beagle/files"
+
+export BEAGLE_PARAM_DIR="${BEAGLE_ROOT}/param"
+export BEAGLE_TEST_FILES="${BEAGLE_ROOT}/tests"
+
+export BEAGLE_TEMPLATES="${BEAGLE_ROOT}/templates"
+export BEAGLE_FILTERS="${BEAGLE_ROOT}/filters"
+export BEAGLE_DUST="${BEAGLE_ROOT}/dust"
+export BEAGLE_DATA="${BEAGLE_ROOT}/data"
+export BEAGLE_SF_CHE="${BEAGLE_ROOT}/sf_che"
+export BEAGLE_RESULTS="${BEAGLE_ROOT}/results"
+
+export BEAGLE_GENERAL="${HOME}/BEAGLE-general"
+
+###################################################
+# END BEAGLE environment variables
+###################################################
---
+++ scripts/BEAGLE_env_variable.csh
@@ -0,0 +1,20 @@
+###################################################
+# START BEAGLE environment variables
+###################################################
+setenv BEAGLE_ROOT "${HOME}/beagle/files"
+
+setenv BEAGLE_PARAM_DIR "${BEAGLE_ROOT}/param"
+setenv BEAGLE_TEST_FILES "${BEAGLE_ROOT}/tests"
+
+setenv BEAGLE_TEMPLATES "${BEAGLE_ROOT}/templates"
+setenv BEAGLE_FILTERS "${BEAGLE_ROOT}/filters"
+setenv BEAGLE_DUST "${BEAGLE_ROOT}/dust"
+setenv BEAGLE_DATA "${BEAGLE_ROOT}/data"
+setenv BEAGLE_SF_CHE "${BEAGLE_ROOT}/sf_che"
+setenv BEAGLE_RESULTS "${BEAGLE_ROOT}/results"
+
+setenv BEAGLE_GENERAL "${HOME}/BEAGLE-general"
+
+###################################################
+# END BEAGLE environment variables
+###################################################
